1ba54b3e473871619974207a83836efd55148d28
enriqueGaleanoT <enriquej999@gmail.com>
2023-07-06 19:11:37 -0600

    showing results on screen and console, while using function in index.html

--- dato.js
+++ dato.js
@@ -3,4 +3,14 @@ let price = 20.5; //float
 let name = 'enrique'; //string
 let character = 'E'; //character
 let booleanExample = true; //boolean
-//Datos primitivos
+
+const number = 23; //Valor que no se puede modificar 
+
+function sum (){
+    let numberA = 5;
+    let numberB = 20;
+    let result = numberA + numberB;
+
+    alert(result);//Muestra output en la consola
+    console.log(result); //Muestra output en pantalla
+}
